e02a53946e031bbc4b642745f8f8ea725223037b
dviviantoro <deny.viviantoro@gmail.com>
2024-06-03 11:41:24 +0700

    feat: add influx client

--- read-serial-py/aio-cerifer.py
+++ read-serial-py/aio-cerifer.py
@@ -1,5 +1,19 @@
 import asyncio
 import aioserial
+from dotenv import load_dotenv
+import influxdb_client, os, time
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+load_dotenv()
+
+token = os.getenv('INFLUX_TOKEN')
+org = os.getenv('ORG')
+url = os.getenv('URL')
+bucket= os.getenv('BUCKET')
+
+client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
+write_api = client.write_api(write_options=SYNCHRONOUS)
 
 class App:
     async def read_serial(self, aioserial_instance: aioserial.AioSerial):
@@ -12,6 +26,22 @@ class App:
             adc_ph = received_data[2]
             volt_battery = received_data[3]
             charge_bettery = received_data[4]
+
+            point_cerifer = (
+                Point("cerifer")
+                # .time(timestamp_influx)
+                .tag("device", device_id)
+                .field("temp", temp)
+                .field("adc_ph", adc_ph)
+                .field("v_bat", volt_battery)
+                .field("c_bat", charge_bettery)
+            )
+
+            try:
+                write_api.write(bucket=bucket, org="nusameta", record=[point_cerifer])
+                print("[SUCCESS] Write data to influxdb")
+            except Exception as e:
+                print(e)
 
 if __name__ == "__main__":
     app = App()
--- read-serial-py/aio-cerigar.py
+++ read-serial-py/aio-cerigar.py
@@ -1,5 +1,19 @@
 import asyncio
 import aioserial
+from dotenv import load_dotenv
+import influxdb_client, os, time
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+load_dotenv()
+
+token = os.getenv('INFLUX_TOKEN')
+org = os.getenv('ORG')
+url = os.getenv('URL')
+bucket= os.getenv('BUCKET')
+
+client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
+write_api = client.write_api(write_options=SYNCHRONOUS)
 
 class App:
     async def read_serial(self, aioserial_instance: aioserial.AioSerial):
@@ -8,12 +22,30 @@ class App:
             print(data.split(";"))
             received_data = data.split(";")
             device_id = received_data[0]
-            bean_temp = received_data[1]
-            lux = received_data[2]
-            ambient_temp = received_data[3]
-            ambient_hum = received_data[4]
-            volt_battery = received_data[5]
-            charge_bettery = received_data[6]
+            bean_temp = float(received_data[1])
+            lux = float(received_data[2])
+            ambient_temp = float(received_data[3])
+            ambient_hum = float(received_data[4])
+            volt_battery = float(received_data[5])
+            charge_bettery = bool(received_data[6])
+
+            point_cerigar = (
+                Point("cerigar")
+                # .time(timestamp_influx)
+                .tag("device", device_id)
+                .field("b_temp", bean_temp)
+                .field("lux", lux)
+                .field("a_temp", ambient_temp)
+                .field("a_hum", ambient_hum)
+                .field("v_bat", volt_battery)
+                .field("c_bat", charge_bettery)
+            )
+
+            try:
+                write_api.write(bucket=bucket, org="nusameta", record=[point_cerigar])
+                print("[SUCCESS] Write data to influxdb")
+            except Exception as e:
+                print(e)
 
 if __name__ == "__main__":
     app = App()
